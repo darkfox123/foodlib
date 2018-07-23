c40f08447f76d902c08fcf9d107549282ac2028c
Shivam Pathak <pathak.shivambareilly@gmail.com>
2018-07-24 02:00:09 +0530

    first commit

--- foodlibrary/src/main/java/com/digitalhomeland/foodlibrary/FoodVeda.java
+++ foodlibrary/src/main/java/com/digitalhomeland/foodlibrary/FoodVeda.java
@@ -1,6 +1,7 @@
 package com.digitalhomeland.foodlibrary;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -10,6 +11,12 @@ public class FoodVeda {
 
         Toast.makeText(c,resId,Toast.LENGTH_SHORT).show();
         Log.d("myTag", " <<<<<<<<<<--- hey man , loaded library : " + resId + " ------->>>>>>>>> ");
-
+    try {
+        Intent intent = new Intent(c, Class.forName("com.abhiandroid.foodorderingin.Activities.SplashScreen"));
+        intent.putExtra("from", "skip");
+        c.startActivity(intent);
+    }catch(Exception e){
+        Log.d("myTag", "error finding class : ", e);
+    }
     }
 }
